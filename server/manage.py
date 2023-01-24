236e9051372760f6fea1148913cec786ffad220d
skllup7 <100334469+skllup7@users.noreply.github.com>
2023-01-24 15:07:30 +0530

    Update manage.py

--- server/manage.py
+++ server/manage.py
@@ -9,8 +9,8 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangobackend.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError:
-        raise ImportError as exc(
+    except ImportError as exc:
+        raise ImportError (
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
